1552189571e222f963f9406a66a9950dd7207e7c
Zhen She <neal.seah@gmail.com>
2020-10-30 16:42:31 +0800

    feat: add solution

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+  const consolidate = _ => _.reduce((acc, cur) => {
+    const found = acc.find(_ => _.Barcode === cur.Barcode)
+    if (found) found.count++
+    else acc.push({...cur, count: 1})
+    return acc
+  }, [])
+
+  const renderItem = _ => `Name: ${_.Name}, Quantity: ${_.count} ${_.Unit+(_.count>1?'s':'')}, Unit price: ${_.Price.toFixed(2)} (yuan), Subtotal: ${(_.Price * _.count).toFixed(2)} (yuan)`
+  const renderItems = items => items.map(renderItem).join('\n')
+  const renderTotal = items => items.map(_ => _.Price * _.count).reduce((a, b)=>a+b, 0).toFixed(2)
+  const renderReceipt = items => '***<store earning no money>Receipt ***\n' +
+      renderItems(items) +
+      '\n----------------------\n' +
+      'Total: '+ renderTotal(items) +' (yuan)\n' +
+      '**********************\n'
+
+  return renderReceipt(consolidate(inputs))
 };
