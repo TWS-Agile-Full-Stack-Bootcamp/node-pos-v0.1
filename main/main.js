2611cc0f9da569186dd9485dcd6fe52e6f760783
Zhen She <neal.seah@gmail.com>
2020-10-30 23:54:30 +0800

    style: separate block by empty line

--- main/main.js
+++ main/main.js
@@ -9,10 +9,11 @@ module.exports = function main(inputs) {
 
   const sum = (a, b) => a + b
   const subtotal = _ => _.Price * _.count
+  const total = items => items.map(subtotal).reduce(sum, 0)
+
   const renderFloat = _ => _.toFixed(2)
   const renderItem = _ => `Name: ${_.Name}, Quantity: ${_.count} ${_.Unit+(_.count>1?'s':'')}, Unit price: ${renderFloat(_.Price)} (yuan), Subtotal: ${renderFloat(subtotal(_))} (yuan)`
   const renderItems = items => items.map(renderItem).join('\n')
-  const total = items => items.map(subtotal).reduce(sum, 0)
   const renderReceipt = items => '***<store earning no money>Receipt ***\n' +
       renderItems(items) +
       '\n----------------------\n' +
