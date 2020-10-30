b69562ad3a4e682b13f18c26b8e0ee96a1428668
Zhen She <neal.seah@gmail.com>
2020-10-30 23:53:26 +0800

    refactor: extract sum

--- main/main.js
+++ main/main.js
@@ -7,11 +7,12 @@ module.exports = function main(inputs) {
     return acc
   }, [])
 
+  const sum = (a, b) => a + b
   const subtotal = _ => _.Price * _.count
   const renderFloat = _ => _.toFixed(2)
   const renderItem = _ => `Name: ${_.Name}, Quantity: ${_.count} ${_.Unit+(_.count>1?'s':'')}, Unit price: ${renderFloat(_.Price)} (yuan), Subtotal: ${renderFloat(subtotal(_))} (yuan)`
   const renderItems = items => items.map(renderItem).join('\n')
-  const total = items => items.map(subtotal).reduce((a, b)=>a+b, 0)
+  const total = items => items.map(subtotal).reduce(sum, 0)
   const renderReceipt = items => '***<store earning no money>Receipt ***\n' +
       renderItems(items) +
       '\n----------------------\n' +
